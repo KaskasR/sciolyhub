50287a0dba42316c380e1b4678363d0990600fb5
Kasra Rategh <kasrarategh@gmail.com>
2025-07-24 17:25:28 -0700

    added keys

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,7 +1,8 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vvhmpwgmwvqcmzomoayj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2aG1wd2dtd3ZxY216b21vYXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzOTc5MTAsImV4cCI6MjA2ODk3MzkxMH0.DY5hCFxfmETmH4GSkvv21pK8VBm9MEFiUPbcsde7iJc'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
