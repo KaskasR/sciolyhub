e03e969dd9ebeca929cd08fc84e04b5a905e5732
Kasra Rategh <kasrarategh@gmail.com>
2025-08-05 11:41:59 -0700

    hopfully fixed routing issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api': 'sciolyhub.vercel.app',
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
